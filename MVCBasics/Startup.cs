fb86611cd7a9a008deee3448b52cff405f3e2b82
Hamza Rahman <hamza.rahman2016@outlook.com>
2021-10-16 16:40:36 +0500

    Begining of React With ASP Net

--- MVCBasics/Startup.cs
+++ MVCBasics/Startup.cs
@@ -11,6 +11,9 @@ using MVCBasics.Models;
 using MVCBasics.Repository;
 using MVCBasics.Services;
 using Newtonsoft.Json.Serialization;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
 
 namespace MVCBasics
 {
@@ -41,7 +44,16 @@ namespace MVCBasics
                 .AddEntityFrameworkStores<PeopleContext>();
 
             services.ConfigureApplicationCookie(options => options.LoginPath = new PathString("/User/Login"));
+
+            services.AddReact();
+
+            // Make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+              .AddV8();
+
             services.AddControllersWithViews();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +70,30 @@ namespace MVCBasics
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
+
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                config
+                  .AddScript("~/js/remarkable.min.js")
+                  .AddScript("~/js/jsx.jsx")
+                  ;
+                //config
+                //  .AddScript("~/js/First.jsx")
+                //  .AddScript("~/js/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+            });
+
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -70,6 +106,7 @@ namespace MVCBasics
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            
         }
     }
 }
---
+++ MVCBasics/Views/Person/People.jsx
@@ -0,0 +1,9 @@
+﻿class PeopleTable extends React.Component {
+    render() {
+        return (
+            <div className="PeopleTable">Hello, world! I am a CommentBox.</div>
+        );
+    }
+}
+
+ReactDOM.render(<PeopleTable />, document.getElementById('PersonTable'));
